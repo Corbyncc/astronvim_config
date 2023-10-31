908053b2822460438c90919ea8bc2186dbec55ff
Corbyn <corbyncummins@protonmail.com>
2023-10-31 09:08:27 -0500

    Add OmniSharp, Modify vim opts, Add tokyonight theme, Show all files in neo-tree

--- options.lua
+++ options.lua
@@ -2,11 +2,16 @@
 return {
   opt = {
     -- set to true or false etc.
+    expandtab = false,
+    smartindent = true,
+    tabstop = 4,
+    shiftwidth = 4,
     relativenumber = true, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    colorcolumn = "80"
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
---
+++ plugins/omnisharp.lua
@@ -0,0 +1,6 @@
+return {
+	"OmniSharp/omnisharp-vim",
+	opts = {
+		OmniSharp_server_path = "/home/corbyn/.local/share/nvim/mason/packages/omnisharp/omnisharp"
+	}
+}
